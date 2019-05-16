e2bf0ab3502815fe3d0ff4c076c1f5f2e113ae4c
vabene1111 <vabene1234@googlemail.com>
2019-05-16 20:04:14 +0200

    split config

---
+++ config/Config_Loadouts.cpp
@@ -0,0 +1,17 @@
+class Loadouts {
+	class black {
+		uniform = "TRYK_U_B_BLKBLK_CombatUniform";
+		vest = "TAC_Jvest_B";
+		backpack = "TAC_LBT_LEGAB2_BK";
+		headgear = "TAC_SF10H";
+		goggles = "TAC_shoulder_armorC_BK";
+	};
+
+	class green {
+		uniform = "U_BG_Guerilla1_2_F";
+		vest = "V_Pocketed_black_F";
+		backpack = "TAC_LBT_LEGAB2_BK";
+		headgear = "H_Shemag_olive";
+		goggles = "TRYK_Shemagh_EAR_G";
+	};
+};
