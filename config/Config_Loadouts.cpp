89d58ee0670c7091c0f48a83c65b5891803b663d
@jannikahoi <jannik@juergensen.email>
2019-05-19 23:54:42 +0200

    added cop uniforms

--- config/Config_Loadouts.cpp
+++ config/Config_Loadouts.cpp
@@ -15,6 +15,30 @@ class Loadouts {
 		goggles = "TRYK_Shemagh_EAR_G";
 	};
 	
+	class sek_cop {
+		uniform = "RL_COP_SEK_1";
+		vest = "RL_V_POLICE_BIG";
+		backpack = "RL_B_COP_BELT";
+		headgear = "RL_H_Police_Helmet";
+		goggles = "TRYK_Spset_PHC1_Glasses";
+	};
+	
+	class zoll_cop {
+		uniform = "RL_Cop_ZOLL";
+		vest = "RL_V_ZOLL";
+		backpack = "RL_B_COP_BELT";
+		headgear = "RL_H_Polizeiberet_zoll";
+		goggles = "TRYK_Spset_PHC1_Glasses";
+	};
+	
+	class cop {
+		uniform = "RL_Cop_PM";
+		vest = "RL_V_Police";
+		backpack = "TAC_LBT_LEGAB2_BK";
+		headgear = "RL_H_Police_Cap_Blue";
+		goggles = "TRYK_Shemagh_EAR_G";
+	};
+	
 	class white {
 		uniform = "TRYK_U_B_Snowt";
 		vest = "TRYK_V_Bulletproof";
